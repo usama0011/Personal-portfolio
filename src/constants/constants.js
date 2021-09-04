44af8bf143f092262820c34175e57853984243ed
usama0011 <za5232208@gmail.com>
2021-09-04 09:42:32 +0500

    first commit

---
+++ src/constants/constants.js
@@ -0,0 +1,46 @@
+export const projects = [
+  {
+    title: 'React Js ',
+    description: "Using React, Node.js and Next js, you'll learn how to build a Full Stack React Application - from start to finish. The App is called Memories and it is a simple social media app that allows users to post interesting events.",
+      image: '/images/1.png',
+      tags: ['React', 'Redux', 'Next', 'FireBase'],
+    source: 'https://google.com',
+    visit: 'https://google.com',
+    id: 0,
+  },
+  {
+    title: 'Next Js',
+    description:"While building it you're going to learn many advanced React & JavaScript , Next js topics, as well as how to use Stripe for card transactions. On top  complex webshop app that you will be able to add to your portfolio.",
+    image: '/images/2.png',
+    tags: ['Next js', 'JavaScript'],
+    source: 'https://google.com',
+    visit: 'https://google.com',
+    id: 1,
+  },
+  {
+    title: 'React User App',
+    description: "This is a code repository for the corresponding YouTube video. In this tutorial, we're going to build and deploy a React Video Chat Application using React FrameWork.",
+      image: '/images/3.jpg',
+      tags: ['React', 'FireBase'],
+    source: 'https://google.com',
+    visit: 'https://google.com',
+    id: 2,
+  },
+  {
+    title: 'Redux Js',
+    description: "This is a code repository for the corresponding video tutorial. In this video, we will create a full Realtime Chat Application",
+    image: '/images/4.jpg',
+    tags: ['React', 'ChatEngine', 'Firebase'],
+    source: 'https://google.com',
+    visit: 'https://google.com',
+    id: 3,
+  },
+];
+
+export const TimeLineData = [
+  { year: 2017, text: 'Started my journey', },
+  { year: 2018, text: 'Worked as a freelance developer', },
+  { year: 2019, text: 'Founded JavaScript and React.js', },
+  { year: 2020, text: 'Shared my projects with the world', },
+  { year: 2021, text: 'Started my own Buseness', },
+];
